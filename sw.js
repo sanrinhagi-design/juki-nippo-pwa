4a1a4864ec88c777376ba2963854fc779a8381ef
sanrinhagi-design <sanrin.hagi@gmail.com>
2026-07-06 05:43:35 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service Worker — キャッシュ & オフライン対応
 // ============================================================
 
-const CACHE_NAME = 'juki-nippo-v10';
+const CACHE_NAME = 'juki-nippo-v11';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
